907e31f3d294ec81caa2d543091dbd65b0b9c6aa
Iryna Burlaka <i.am.blinova@gmail.com>
2024-03-05 13:52:56 +0100

    more settings

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+// const utils = require("./utils");
+
+// import app from "./app.js";
+
+app.listen(3001, () => {
+  //   utils.prepareEnvironment();
+  console.log("Server running. Use our API on port: 3001");
+});
