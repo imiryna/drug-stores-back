7d2defc5f448d71e2d7b3d291b3cc99a8376adf8
Iryna Burlaka <i.am.blinova@gmail.com>
2024-03-06 13:41:54 +0100

    dotenv configur

--- services/firebaseDB.js
+++ services/firebaseDB.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: "drug-stor.firebaseapp.com",
-  databaseURL: "https://drug-stor-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: process.env.DATABASE_URL,
   projectId: "drug-stor",
   storageBucket: "drug-stor.appspot.com",
   messagingSenderId: "561596546010",
-  appId: "1:561596546010:web:c83efab809d1fbec1ddee3",
+  appId: process.env.APP_ID,
   measurementId: "G-H0K3GM6MKE",
 };
 
